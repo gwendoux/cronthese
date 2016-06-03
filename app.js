2f6d32f01d32ec7f7ac6ec19983d375495ea919e
Gwen Doux <gwendoux@gmail.com>
2016-06-03 10:39:06 +0200

    modified app config

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 
 const config = require('./lib/config');
 const logger = config.getLogger();
-const schedule = require('node-schedule');
+// const schedule = require('node-schedule');
 const request = require('request-promise');
 
 const Pinboard_API_Token = config.get('pinboard_API_TOKEN');
-const Pinboard_API_Endpoint = 'https://api.pinboard.in/v1/posts/recent?auth_token=' + Pinboard_API_Token + '&format=json&count=12';
+const Pinboard_API_Endpoint = config.get('Pinboard_API_ENDPOINT');
 
+var Pinboard_Options = {
+    uri: Pinboard_API_Endpoint,
+    qs: {
+        auth_token: Pinboard_API_Token,
+        format: 'json',
+        count: '12'
+    }
+};
 
+// commented out when ready to run
 // schedule.scheduleJob('*/2 * * * *', function(){
-    request(Pinboard_API_Endpoint)
+request(Pinboard_Options)
       .then(function (data) {
-          var content = JSON.parse(data).posts;
+          return JSON.parse(data).posts;
+      }).then(function(content) {
           logger.info(content);
-      })
-      .catch(function (err) {
+      }).catch(function (err) {
           logger.error(err.message);
       });
 // });
