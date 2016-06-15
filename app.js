3dddcbf6db7c67fc0b3320bcd4199e54b1a5b8d8
Gwenaël Doux <gwendoux@gmail.com>
2016-06-15 10:06:25 +0200

    activated schedule

--- app.js
+++ app.js
@@ -10,29 +10,28 @@ const actions = require('./lib/actions');
 const filter = require('./lib/filter');
 
 // schedule for photos every 2 minutes
-// schedule.scheduleJob('*/2 * * * *', function(){
-request(appConfig.pinboard).then(function (resp) {
-    return JSON.parse(resp).posts;
-}).then(function(content) {
-    content.forEach(function(item) {
-        actions.saveNew(db.Links, build.schemaLinks(db.Links, item), item.hash);
+schedule.scheduleJob('*/2 * * * *', function(){
+    request(appConfig.pinboard).then(function (resp) {
+        return JSON.parse(resp).posts;
+    }).then(function(content) {
+        content.forEach(function(item) {
+            actions.saveNew(db.Links, build.schemaLinks(db.Links, item), item.hash);
+        });
+    }).catch(function (err) {
+        logger.error(err.message);
     });
-}).catch(function (err) {
-    logger.error(err.message);
 });
-// });
 // schedule for photos every 30 minutes
-// schedule.scheduleJob('* */30 * * *', function(){
-request(appConfig.instagram).then(function (resp) {
-    return JSON.parse(resp).data;
-}).then(function(content) {
-    return (filter.filterByTag(content, 'coffeeoftheday'));
-}).then(function(filteredContent) {
-    filteredContent.forEach(function(item) {
-        actions.saveNew(db.Coffee, build.schemaCoffee(db.Coffee, item), item.id);
+schedule.scheduleJob('* */30 * * *', function(){
+    request(appConfig.instagram).then(function (resp) {
+        return JSON.parse(resp).data;
+    }).then(function(content) {
+        return (filter.filterByTag(content, 'coffeeoftheday'));
+    }).then(function(filteredContent) {
+        filteredContent.forEach(function(item) {
+            actions.saveNew(db.Coffee, build.schemaCoffee(db.Coffee, item), item.id);
+        });
+    }).catch(function (err) {
+        logger.error(err.message);
     });
-}).catch(function (err) {
-    logger.error(err.message);
 });
-// });
-// });
